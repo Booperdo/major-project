9db3268d62e6b774ec4f0edf21e64f5987dc760f
Booperdo <77638125+Booperdo@users.noreply.github.com>
2021-02-09 16:04:08 -0600

    Finished Proposal and Created game board.

--- proposal.md
+++ proposal.md
@@ -1,7 +1,16 @@
 # Proposal
-here is were i describe what im going to build
+I am going to make a game of checkers. There will be a single player and two player game modes.
 
 ## Needs to Have
-
+-start screen
+-game board
+-two different teams(Game pieces)
+-ai to play single player 
+-multiplayer
 
 ## Nice to Have
+-timer 
+-different sized boards
+-move counter
+-move display
+-rule book
--- sketch.js
+++ sketch.js
@@ -1,15 +1,61 @@
-// Project Title
-// Your Name
-// Date
+// Major Project
+// Cooper
+// Tues 9/21
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+const ROWS = 12;
+const COLS = 12;
+let grid, cellWidth, cellHeight;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let myCanvas = createCanvas(windowWidth*0.8, windowHeight*0.8);
+  myCanvas.position(windowWidth*0.1, windowHeight*0.1);
+  grid = createEmptyGrid(COLS, ROWS);
+  cellWidth = width / COLS;
+  cellHeight = height / ROWS;
 }
 
 function draw() {
-  background(220);
+  background(255);
+  displayGrid();
+}
+
+function createEmptyGrid(cols, rows) {
+  let empty = [];
+  let number = 0;
+  for (let y=0; y<rows; y++) {
+    empty.push([]);
+    for (let x=0; x<cols; x++) {
+      empty[y].push(number);
+      if (number === 0) {
+        number = 1;
+      }
+      else if (number === 1) {
+        number = 0;
+      }
+    }
+    if (number === 0) {
+      number = 1;
+    }
+    else if (number === 1) {
+      number = 0;
+    }
+  }
+  return empty;
+}
+
+function displayGrid() {
+  for (let y=0; y<ROWS; y++) {
+    for (let x=0; x<COLS; x++) {
+      if (grid[y][x] === 0) {
+        fill("black");
+      }
+      else {
+        fill("white");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
 }
