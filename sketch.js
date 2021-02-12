442af7335d56306e4007387c8891d48beadccf8f
Booperdo <77638125+Booperdo@users.noreply.github.com>
2021-02-12 12:54:27 -0600

    created game pieces

--- sketch.js
+++ sketch.js
@@ -7,20 +7,40 @@
 
 const ROWS = 12;
 const COLS = 12;
-let grid, cellWidth, cellHeight;
+let grid, cellWidth, cellHeight, boardRows, boardCols;
+
+let board = 
+[[0,1,0,1,0,1,0,1,0,1,0,1], 
+  [1,0,1,0,1,0,1,0,1,0,1,0],
+  [0,1,0,1,0,1,0,1,0,1,0,1],
+  [1,0,1,0,1,0,1,0,1,0,1,0],
+  [0,1,0,1,0,1,0,1,0,1,0,1],
+  [0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0],
+  [1,0,1,0,1,0,1,0,1,0,1,0],
+  [0,1,0,1,0,1,0,1,0,1,0,1],
+  [1,0,1,0,1,0,1,0,1,0,1,0],
+  [0,1,0,1,0,1,0,1,0,1,0,1],
+  [1,0,1,0,1,0,1,0,1,0,1,0]
+];
 
 function setup() {
-  let myCanvas = createCanvas(windowWidth*0.8, windowHeight*0.8);
-  myCanvas.position(windowWidth*0.1, windowHeight*0.1);
+  let myCanvas = createCanvas(windowWidth*0.5, windowHeight*1);
+  myCanvas.position(windowWidth*0.25, windowHeight*0);
   grid = createEmptyGrid(COLS, ROWS);
   cellWidth = width / COLS;
   cellHeight = height / ROWS;
+
+  boardRows = board.length;
+  boardCols = board[0].length;
 }
 
 function draw() {
   background(255);
   displayGrid();
+  displayPieces();
 }
+
 
 function createEmptyGrid(cols, rows) {
   let empty = [];
@@ -46,14 +66,31 @@ function createEmptyGrid(cols, rows) {
   return empty;
 }
 
+function displayPieces() {
+  for (let y=0; y<boardRows; y++) {
+    for (let x=0; x<boardCols; x++) {
+      noStroke();
+      if (board[y][x] === 1) {
+        fill("red");
+      }
+      else {
+        noFill();
+      }
+      ellipseMode(CORNER);
+      ellipse(x*cellWidth,y*cellHeight,cellWidth-1,cellHeight-1);
+    }
+  }
+}
+
+
 function displayGrid() {
   for (let y=0; y<ROWS; y++) {
     for (let x=0; x<COLS; x++) {
       if (grid[y][x] === 0) {
-        fill("black");
+        fill("white");
       }
       else {
-        fill("white");
+        fill("black");
       }
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
